53fec9080f72c0b8697a822bff1a926f21b5b553
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2025-08-07 22:50:53 -0300

    Atualização de arquivo

--- sirius-docs/src/components/HomepageFeatures/index.js
+++ sirius-docs/src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Sirius Hub',
+    title: 'Sirius Hub Education',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
